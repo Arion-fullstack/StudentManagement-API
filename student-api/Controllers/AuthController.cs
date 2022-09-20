8aed6b8b4a636887b30bc6ced44979b18c5b49bc
loidukezzz <102323043+loidukezzz@users.noreply.github.com>
2022-09-20 21:15:19 +0700

    Update AuthController

--- student-api/Controllers/AuthController.cs
+++ student-api/Controllers/AuthController.cs
@@ -10,6 +10,57 @@ namespace student_api.Controllers
 
     public class AuthController : ControllerBase
     {
-        
+        private readonly IAuthService _authService;
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+
+        [HttpGet]
+        public IActionResult GetAllUser()
+        {
+            using (StudentManagementContext db = new StudentManagementContext())
+            {
+                var users = db.Users.ToList();
+                return Ok(users);
+            }
+        }
+
+        [Route("login")]
+        [HttpPost]
+        public IActionResult Login(UserRequest user)
+        {
+            var loginService = _authService.Login(user);
+            return StatusCode(loginService.Code, loginService);
+        }
+
+        [Route("register")]
+        [HttpPost]
+        public async Task<IActionResult> Register(UserRequest user)
+        {
+            var registerService = await _authService.Register(user);
+            return StatusCode(registerService.Code, registerService);
+        }
+
+        [Route("refresh-token")]
+        [HttpPost]
+        public async Task<IActionResult> RefreshToken()
+        {
+            var refreshTokenStr = Request.Headers["refreshToken"];
+            var refreshTokenService = await _authService.RefreshToken(refreshTokenStr);
+            if (refreshTokenService.Code == 200)
+                Response.Headers.Add("refreshToken", refreshTokenService.Data.RefreshToken);
+            return StatusCode(200, refreshTokenService);
+        }
+
+        [Route("logout")]
+        [HttpPost, Authorize]
+        public async Task<IActionResult> Logout()
+        {
+            var refreshTokenStr = Request.Headers["refreshToken"];
+            var LogoutService = await _authService.Logout(refreshTokenStr);
+            return StatusCode(200, LogoutService);
+        }
     }
 }
